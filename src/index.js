3cdbea057c08a83372173aa391244d9f477ad5ea
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-12-21 08:37:56 +0700

    merge

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import { load, save } from "redux-localstorage-simple";
 import thunk from "redux-thunk";
 import "~/assets/scss/style.scss";
 import App from "./App";
-// import products from "./data/products.json";
-// import { fetchProducts } from "./redux/actions/productActions";
-// import rootReducer from "./redux/reducers/rootReducer";
+import products from "./data/products.json";
+import { fetchProducts } from "./redux/actions/productActions";
+import rootReducer from "./redux/reducers/rootReducer";
 
-// const store = createStore(rootReducer, load(), applyMiddleware(thunk, save()));
+const store = createStore(rootReducer, load(), applyMiddleware(thunk, save()));
 
-// store.dispatch(fetchProducts(products));
+store.dispatch(fetchProducts(products));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
